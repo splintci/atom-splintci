72fbaf8e882999a084a0be511534e91aaecaa421
Francis Ilechukwu <francis94c@gmail.com>
2019-09-04 13:57:35 +0100

    Initial Commit.

---
+++ spec/atom-splintci-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSplintciView from '../lib/atom-splintci-view';
+
+describe('AtomSplintciView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
